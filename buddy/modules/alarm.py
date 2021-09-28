757ac20e50a65971d452f00ba1fc23114d214062
Soul Queen <89896200+ImSoulQueen@users.noreply.github.com>
2021-09-28 15:30:39 +0530

    Create alarm.py

---
+++ buddy/modules/alarm.py
@@ -0,0 +1,10 @@
+__help__ = """
+Do your works at time, don't late.
+*Commands*
+ • `/setalarm <(date) (time)|zone|reason>`*:* sets a alarm/reminder 
+*Syntax:* `/setalarm 01/01/2000 10:00:00 AM | Asia/Kolkata | breakfast`
+*NOTE:*
+Please turn on notifications(PM/Group Chat) otherwise you will not get notification for the alarm!
+"""
+
+__mod_name__ = "ALARM⏰"
