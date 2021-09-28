1664520f7e26b9c6832775f4ccedfd494e3f7768
Soul Queen <89896200+ImSoulQueen@users.noreply.github.com>
2021-09-28 12:28:08 +0530

    Update disable.py

--- buddy/modules/disable.py
+++ buddy/modules/disable.py
@@ -17,8 +17,8 @@ if is_module_loaded(FILENAME):
 
     from telegram.ext.dispatcher import run_async
 
-    from cinderella.modules.helper_funcs.chat_status import user_admin, is_user_admin, connection_status
-    from cinderella.modules.sql import disable_sql as sql
+    from buddy.modules.helper_funcs.chat_status import user_admin, is_user_admin, connection_status
+    from buddy.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
@@ -118,7 +118,7 @@ if is_module_loaded(FILENAME):
     def disable_module(bot: Bot, update: Update, args: List[str]):
         chat = update.effective_chat
         if len(args) >= 1:
-            disable_module = "cinderella.modules." + args[0].rsplit(".", 1)[0]
+            disable_module = "buddy.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(disable_module)
@@ -187,7 +187,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
 
         if len(args) >= 1:
-            enable_module = "cinderella.modules." + args[0].rsplit(".", 1)[0]
+            enable_module = "buddy.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(enable_module)
